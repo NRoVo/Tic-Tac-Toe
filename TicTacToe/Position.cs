0b0fbae9fdda0d55898de511ef1bc5e823d4905b
Igor <nrovo7@gmail.com>
2020-12-12 23:36:05 +0200

    Initial commit of TicTacToe.

---
+++ TicTacToe/Position.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Position
+    {
+        public int Row { get; }
+        public int Column { get; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+}
