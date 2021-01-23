55a1d64d8b186e178a839c1aafa1ce2e977c3a25
Igor <nrovo7@gmail.com>
2021-01-23 19:55:34 +0200

    no message

--- TicTacToe/Renderer.cs
+++ TicTacToe/Renderer.cs
@@ -1,49 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TicTacToe
 {
     public class Renderer
     {
-        public void Render(Board board)
-        {
-            char[,] symbols = new char[3, 3];
+        private readonly Board _board;
 
-            for (int row = 0; row < 3; row++)
+        public Renderer(Board board)
+        {
+            this._board = board;
+        }
+
+        public void Render()
+        {
+            var symbols = new char[3, 3];
+            for (var row = 0; row < 3; row++)
+            for (var column = 0; column < 3; column++)
             {
-                for (int column = 0; column < 3; column++)
-                {
-                    symbols[row, column] = SymbolFor(board.GetState(new Position(row, column)));
-                }                
+                symbols[row, column] = SymbolFor(_board.GetState(new Position(row, column)));
             }
-            Console.WriteLine($" {symbols[0, 0]} | {symbols[0, 1]} | {symbols[0, 2]}");
+
+            Console.WriteLine($" {symbols[0, 0]} | {symbols[0, 1]} | {symbols[0, 2]} ");
             Console.WriteLine("---+---+---");
-            Console.WriteLine($" {symbols[1, 0]} | {symbols[1, 1]} | {symbols[1, 2]}");
+            Console.WriteLine($" {symbols[1, 0]} | {symbols[1, 1]} | {symbols[1, 2]} ");
             Console.WriteLine("---+---+---");
-            Console.WriteLine($" {symbols[2, 0]} | {symbols[2, 1]} | {symbols[2, 2]}");
+            Console.WriteLine($" {symbols[2, 0]} | {symbols[2, 1]} | {symbols[2, 2]} ");
         }
 
         private char SymbolFor(State state)
         {
-            switch(state)
+            switch (state)
             {
-                case State.O:
-                    return 'O';                    
-                case State.X:
-                    return 'X';
-                default:
-                    return ' ';
+                case State.O: return 'O';
+                case State.X: return 'X';
+                default:      return ' ';
             }
         }
 
         public void RenderResults(State winner)
         {
-            switch(winner)
+            switch (winner)
             {
-                case State.O:                    
+                case State.O:
                 case State.X:
                     Console.WriteLine(SymbolFor(winner) + " Wins!");
                     break;
